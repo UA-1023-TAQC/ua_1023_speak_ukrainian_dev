0e08d3557e6438442e24d84cabd4e304e7646dcd
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-11-09 11:42:22 +0200

    completed methods for faq section

--- src/main/java/com/softserveinc/speakukrainian/pageobjects/ServicesInUkrainianPage.java
+++ src/main/java/com/softserveinc/speakukrainian/pageobjects/ServicesInUkrainianPage.java
@@ -5,35 +5,59 @@ import com.codeborne.selenide.SelenideElement;
 
 public class ServicesInUkrainianPage {
 
-    public SelenideElement getFAQTitle() {
+    public SelenideElement getFAQTitleElement() {
         return Selenide.$x("//div[@class='faq-title']");
     }
 
     public String getFAQTitleText() {
-        return getFAQTitle().getText();
+        return getFAQTitleElement().getText();
     }
 
-    public SelenideElement getFAQQuestionServiceDenial() {
+    public SelenideElement getFAQQuestionServiceDenialElement() {
         return Selenide.$x("//div[@class='ant-collapse-item panel'][1]");
     }
 
-    public String getFAQQuestionServiceDenialText() {
-        return getFAQQuestionServiceDenial().getText();
+    public String getFAQQuestionServiceDenialTitleText() {
+        return getFAQQuestionServiceDenialElement()
+                .$x("//div[@class='ant-collapse-header-text']")
+                .getText();
     }
 
-    public SelenideElement getFAQQuestionWhereToComplain() {
+    public String getFAQQuestionServiceDenialContentText() {
+        return getFAQQuestionServiceDenialElement()
+                .$x(".//div[@class='ant-collapse-content-box']/p")
+                .getText();
+    }
+
+    public SelenideElement getFAQQuestionWhereToComplainElement() {
         return Selenide.$x("//div[@class='ant-collapse-item panel'][2]");
     }
 
-    public String getFAQQuestionWhereToComplainText() {
-        return getFAQQuestionWhereToComplain().getText();
+    public String getFAQQuestionWhereToComplainTitleText() {
+        return getFAQQuestionWhereToComplainElement()
+                .$x("//div[@class='ant-collapse-header-text']")
+                .getText();
     }
 
-    public SelenideElement getFAQQuestionWhatComplainContains() {
+    public String getFAQQuestionWhereToComplainContentText() {
+        return getFAQQuestionWhereToComplainElement()
+                .$x(".//div[@class='ant-collapse-content-box']/p")
+                .getText();
+    }
+
+    public SelenideElement getFAQQuestionWhatComplaintContainsElement() {
         return Selenide.$x("//div[@class='ant-collapse-item panel'][3]");
     }
 
-    public String getFAQQuestionWhatComplainContainsText() {
-        return getFAQQuestionWhatComplainContains().getText();
+    public String getFAQQuestionWhatComplainContainsTitleText() {
+        return getFAQQuestionWhatComplaintContainsElement()
+                .$x("//div[@class='ant-collapse-header-text']")
+                .getText();
+    }
+
+    public String getFAQQuestionWhatComplainContainsContentText() {
+        return getFAQQuestionWhatComplaintContainsElement()
+                .$x(".//div[@class='ant-collapse-content-box']/p")
+                .getText();
     }
 }
