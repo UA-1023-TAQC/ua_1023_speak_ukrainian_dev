08c884165ee365c2fe0c3f9aedd855e7c57d4f4e
Olha Haichenkova <olha.haichenkova@gmail.com>
2023-11-14 16:16:06 +0200

    added changes from dev

--- src/main/java/com/softserveinc/speakukrainian/pageobjects/BasePage.java
+++ src/main/java/com/softserveinc/speakukrainian/pageobjects/BasePage.java
@@ -1,10 +1,16 @@
 package com.softserveinc.speakukrainian.pageobjects;
 
+import com.softserveinc.speakukrainian.pageobjects.components.Footer;
+import com.softserveinc.speakukrainian.pageobjects.components.Header;
 import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Selenide.actions;
 
 public abstract class BasePage {
+
+    protected static final Header HEADER = new Header();
+    protected static final Footer FOOTER = new Footer();
+
 
     public void pressArrowDown(int count) {
         for (int i=0; i<count; i++) {
