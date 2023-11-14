2a97b4a88ec44c570a30ca9b6e2ac79aa52db25d
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-11-14 18:27:16 +0200

    implemented ChallengeMenu

--- src/main/java/com/softserveinc/speakukrainian/pageobjects/components/ChallengeMenu.java
+++ src/main/java/com/softserveinc/speakukrainian/pageobjects/components/ChallengeMenu.java
@@ -1,5 +1,34 @@
 package com.softserveinc.speakukrainian.pageobjects.components;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import com.softserveinc.speakukrainian.pageobjects.challenges.ChallengeMovomarathonPage;
+
+import static com.codeborne.selenide.Selenide.$x;
+
 // TODO: implement
 public class ChallengeMenu {
+
+    private final SelenideElement menu;
+
+    public ChallengeMenu() {
+        this.menu = $x("//ul[contains(@id, 'challenge']");
+    }
+
+    public ElementsCollection getListOfMenuItems() {
+        return menu.$$("li");
+    }
+
+    public String getTextOfMenuItem(int index) {
+        return getListOfMenuItems().get(index).$x(".//a").getText();
+    }
+
+    public String getLinkOfMenuItem(int index) {
+        return getListOfMenuItems().get(index).$x(".//a").getAttribute("href");
+    }
+
+    public ChallengeMovomarathonPage openMovomarathonChallenge() {
+        menu.$x(".//a[contains(text(), 'Мовомарафон')]").click();
+        return new ChallengeMovomarathonPage();
+    }
 }
