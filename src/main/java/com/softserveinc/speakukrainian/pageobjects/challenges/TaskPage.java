9653f9cb4117264a5cf3925343c87662dac7cfd9
Olha Haichenkova <olha.haichenkova@gmail.com>
2023-11-16 12:39:55 +0200

    fix

--- src/main/java/com/softserveinc/speakukrainian/pageobjects/challenges/TaskPage.java
+++ src/main/java/com/softserveinc/speakukrainian/pageobjects/challenges/TaskPage.java
@@ -2,11 +2,14 @@ package com.softserveinc.speakukrainian.pageobjects.challenges;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import com.softserveinc.speakukrainian.pageobjects.BasePage;
+import lombok.Getter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class TaskPage {
+@Getter
+public class TaskPage extends BasePage {
 
     private final SelenideElement taskTitle = $("div.task-header");
     private final SelenideElement taskImage = $("img.task-image");
