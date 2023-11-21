9680872b1acbaddcb07f8e7fc7bcd57687ef3390
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-11-21 10:18:55 +0200

    created test runner with admin

---
+++ src/main/java/com/softserveinc/speakukrainian/utils/TestRunnerWithAdmin.java
@@ -0,0 +1,21 @@
+package com.softserveinc.speakukrainian.utils;
+
+import org.testng.annotations.BeforeMethod;
+
+public class TestRunnerWithAdmin extends TestRunner {
+
+    @BeforeMethod
+    public void loginAsAdmin() {
+        {
+            homePage.open();
+            homePage
+                    .getHeader()
+                    .openGuestProfileMenu()
+                    .openLogin()
+                    .setEmailInput(PropertyUtil.getAdminEmail())
+                    .setPasswordInput(PropertyUtil.getAdminPassword())
+                    .clickSubmitBtn();
+        }
+    }
+
+}
