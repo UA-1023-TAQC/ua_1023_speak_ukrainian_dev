87e573326e312e8b0722e25973bc212435555c63
Natali-natali <rozdilska.n@gmail.com>
2023-11-23 13:45:35 +0200

    update

--- src/test/java/com/softserveinc/speakukrainian/MovomarathonPage.java
+++ src/test/java/com/softserveinc/speakukrainian/MovomarathonPage.java
@@ -1,19 +1,20 @@
 package com.softserveinc.speakukrainian;
+
 import com.softserveinc.speakukrainian.pageobjects.homePage.HomePage;
 import com.softserveinc.speakukrainian.utils.TestRunner;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
 
-public class MovomarathonPage extends TestRunner{
+public class MovomarathonPage extends TestRunner {
 
-        @Test
-        public void verifyChallengeMovomarathonPageIsDisplayed() {
-            new HomePage()
-                    .getHeader()
-                    .openChallengeMenu()
-                    .openMovomarathonChallenge()
-                    .getPageTitle()
-                    .shouldHave(text("Мовомаратон «30 років - 30 кроків»"));
-        }
+    @Test
+    public void verifyChallengeMovomarathonPageIsDisplayed() {
+        new HomePage()
+                .getHeader()
+                .openChallengeMenu()
+                .openMovomarathonChallenge()
+                .getPageTitle()
+                .shouldHave(text("Мовомаратон «30 років - 30 кроків»"));
+    }
 }
