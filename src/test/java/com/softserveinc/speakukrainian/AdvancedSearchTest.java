c1288d36e38c30ed0edd203e5f9311dc316870a7
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-11-28 12:11:15 +0200

    added test for advanced search

--- src/test/java/com/softserveinc/speakukrainian/AdvancedSearchTest.java
+++ src/test/java/com/softserveinc/speakukrainian/AdvancedSearchTest.java
@@ -9,6 +9,7 @@ import org.testng.asserts.SoftAssert;
 public class AdvancedSearchTest extends TestRunner {
 
     @Test
+    // https://jira.softserve.academy/projects/TUA/issues/TUA-510
     public void centerRadiobuttonDisablesParameters(){
 
         AdvancedSearch advancedSearch = new HomePage()
@@ -23,6 +24,28 @@ public class AdvancedSearchTest extends TestRunner {
         softAssert.assertFalse(advancedSearch.isAgeInputPresent());
         softAssert.assertFalse(advancedSearch.isRemoteCheckboxPresent());
         softAssert.assertFalse(advancedSearch.isCategoriesCheckboxesBlockPresent());
+
+        softAssert.assertAll();
+    }
+
+    @Test
+    //https://jira.softserve.academy/projects/TUA/issues/TUA-509
+    public void clubRadiobuttonEnablesParameters(){
+
+        AdvancedSearch advancedSearch = new HomePage()
+                .getHeader()
+                .clickAdvancedSearchBtn();
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(advancedSearch.isClubRadioButtonSelected());
+        softAssert.assertFalse(advancedSearch.isCentreRadioButtonSelected());
+        softAssert.assertTrue(advancedSearch.isCityDropDownPresent());
+        softAssert.assertTrue(advancedSearch.isDistrictDropDownPresent());
+        softAssert.assertTrue(advancedSearch.isSubwayStationDropDownPresent());
+        softAssert.assertTrue(advancedSearch.isAgeInputPresent());
+        softAssert.assertTrue(advancedSearch.isRemoteCheckboxPresent());
+        softAssert.assertTrue(advancedSearch.isCategoriesCheckboxesBlockPresent());
 
         softAssert.assertAll();
     }
