e654366a3c80f0f5e5ad9bf33ce19908faef82f8
Natali-natali <rozdilska.n@gmail.com>
2023-12-03 23:57:29 +0200

    update locators

--- src/main/java/com/softserveinc/speakukrainian/pageobjects/components/AdvancedSearch/SortingBlockComponent.java
+++ src/main/java/com/softserveinc/speakukrainian/pageobjects/components/AdvancedSearch/SortingBlockComponent.java
@@ -3,6 +3,8 @@ package com.softserveinc.speakukrainian.pageobjects.components.AdvancedSearch;
 import com.codeborne.selenide.SelenideElement;
 import com.softserveinc.speakukrainian.pageobjects.ClubsPage.ClubsPage;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class SortingBlockComponent {
 
     public SelenideElement node;
@@ -11,12 +13,12 @@ public class SortingBlockComponent {
     }
 
     public ClubsPage clickSortByAlphabetOrRating(String sortBy) {
-        this.node.$x(".//span[contains(text(), '" + sortBy + "')]").click();
+        this.node.$x(String.format(".//span[contains(text(), '%s')]", sortBy)).click();
         return new ClubsPage();
     }
 
     public ClubsPage clickArrowUpOrDown(String arrowUpOrDown){
-        this.node.$x(".//span[@aria-label='" + arrowUpOrDown + "']").click(); //arrow-up or arrow-down
+        this.node.$x(String.format(".//span[@aria-label='%s']", arrowUpOrDown)).click(); //arrow-up or arrow-down
         return new ClubsPage();
     }
 }
