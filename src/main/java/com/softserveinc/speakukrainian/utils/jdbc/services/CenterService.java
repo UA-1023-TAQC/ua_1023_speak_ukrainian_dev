314e2a63dc9c599a3120de8b7ad3e5ab5abb4458
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-12-05 20:07:24 +0200

    created CenterService

---
+++ src/main/java/com/softserveinc/speakukrainian/utils/jdbc/services/CenterService.java
@@ -0,0 +1,19 @@
+package com.softserveinc.speakukrainian.utils.jdbc.services;
+
+import com.softserveinc.speakukrainian.utils.jdbc.dao.CenterDAO
+import com.softserveinc.speakukrainian.utils.jdbc.entity.CenterEntity;
+
+import java.util.List;
+
+public class CenterService {
+    private final CenterDAO centerDAO;
+
+    public CenterService(){
+        centerDAO = new CenterDAO();
+    }
+
+    public List<CenterEntity> getFirstSixCentersAscendingByName(){
+        return centerDAO.selectFirsSixCentersAscendingByName();
+    }
+
+}
