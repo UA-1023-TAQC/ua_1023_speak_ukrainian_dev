22ee1ae983621c1c0fcaa7c65257c5c08f8884ff
Natali-natali <rozdilska.n@gmail.com>
2023-12-06 00:39:57 +0200

    update

--- src/test/java/com/softserveinc/speakukrainian/ui/guest/HomePageTest.java
+++ src/test/java/com/softserveinc/speakukrainian/ui/guest/HomePageTest.java
@@ -1,14 +1,8 @@
 package com.softserveinc.speakukrainian.ui.guest;
 
-
-import com.softserveinc.speakukrainian.pageobjects.components.AdvancedSearch.AdvancedSearch;
-import com.softserveinc.speakukrainian.pageobjects.homePage.HomePage;
 import com.softserveinc.speakukrainian.utils.TestRunner;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Condition.visible;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
 
 public class HomePageTest extends TestRunner {
 
