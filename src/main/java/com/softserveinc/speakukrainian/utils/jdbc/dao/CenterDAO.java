5e6342a49fc700dfeff4b66b7c69e43c2e57c512
Yaroslav Prystai <yaroslav.prystai@gmail.com>
2023-12-05 20:06:36 +0200

    created CenterDAO

---
+++ src/main/java/com/softserveinc/speakukrainian/utils/jdbc/dao/CenterDAO.java
@@ -0,0 +1,24 @@
+package com.softserveinc.speakukrainian.utils.jdbc.dao;
+
+import com.softserveinc.speakukrainian.utils.jdbc.entity.CenterEntity;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class CenterDAO {
+    public List<CenterEntity> selectFirsSixCentersAscendingByName() {
+        Statement statement = ManagerDAO.getInstance().getStatement();
+        List<List<String>> rows = null;
+        try {
+            ResultSet resultSet = statement.executeQuery(CenterEntity.SELECT_FIRST_SIX_CENTERS_ASC);
+            rows = ManagerDAO.getInstance().parseResultSet(resultSet);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        ManagerDAO.closeStatement(statement);
+        return CenterEntity.parseRows(rows);
+
+    }
+}
